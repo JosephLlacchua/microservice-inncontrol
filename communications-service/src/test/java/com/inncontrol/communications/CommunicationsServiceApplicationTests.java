f2d8506ce0aa341478dee1b2cf6fe2870116814b
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-10-10 04:19:28 -0500

    Initial commit

---
+++ communications-service/src/test/java/com/inncontrol/communications/CommunicationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inncontrol.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-service/src/test/java/com/inncontrol/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inncontrol.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
