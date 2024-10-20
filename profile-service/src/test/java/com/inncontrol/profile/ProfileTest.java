5e5163b2bbf61de4729d10b5c0ba24291309fc74
SharonBarrial <u202114900@upc.edu.pe>
2024-10-19 21:16:57 -0500

    feat: create profile test (profile test)
    Co-Authored-By: Anthony Guerrero Castillo <89022078+AnthonyGuerrero56@users.noreply.github.com>

---
+++ profile-service/src/test/java/com/inncontrol/profile/ProfileTest.java
@@ -0,0 +1,16 @@
+package com.inncontrol.profile;
+
+import com.inncontrol.profile.domain.model.aggregates.Profile;
+import com.inncontrol.profile.domain.model.commands.CreateProfileCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Sharon Antuanet Ivet Barrial Marin - u202114900
+ * @version 1.0
+ */
+
+class ProfileTest {
+
+    
+}
