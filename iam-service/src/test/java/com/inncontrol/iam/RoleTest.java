dcc7c44b1bd1e2f5ade51a4ab33569b56a05d2f8
VittorioEdu <u20231b547@upc.edu.pe>
2024-10-19 20:11:30 -0500

    feat: Create Role Test

---
+++ iam-service/src/test/java/com/inncontrol/iam/RoleTest.java
@@ -0,0 +1,21 @@
+package com.inncontrol.iam;
+
+
+import com.inncontrol.iam.domain.model.entities.Role;
+import com.inncontrol.iam.domain.model.valueobjects.Roles;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleTest {
+
+    @Test
+    void getDefaultRole_ShouldReturnEmployeeRole() {
+        Role defaultRole = Role.getDefaultRole();
+        assertNotNull(defaultRole);
+        assertEquals(Roles.EMPLOYEE, defaultRole.getName());
+    }
+
+}
