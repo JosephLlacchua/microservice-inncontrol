57bbac88471bb18b2b4667231bd0879f295b2876
SharonBarrial <u202114900@upc.edu.pe>
2024-10-19 19:26:20 -0500

    feat: create user test (UserTest)

---
+++ iam-service/src/test/java/com/inncontrol/iam/UserTest.java
@@ -0,0 +1,22 @@
+package com.inncontrol.iam;
+
+import com.inncontrol.iam.domain.model.aggregates.User;
+import com.inncontrol.iam.domain.model.entities.Role;
+import com.inncontrol.iam.domain.model.valueobjects.Roles;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Sharon Antuanet Ivet Barrial Marin - u202114900
+ * @version 1.0
+ */
+
+@SpringBootTest
+public class UserTest {
+
+}
