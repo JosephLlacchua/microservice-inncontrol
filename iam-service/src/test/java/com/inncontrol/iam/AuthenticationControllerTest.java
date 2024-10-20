5f70e13b1d0009c0f9238ad370315ac8881220a3
ern23zc <U202112907@upc.edu.pe>
2024-10-19 19:42:57 -0500

    feat: Added dependencies for test

--- iam-service/src/test/java/com/inncontrol/iam/AuthenticationControllerTest.java
+++ iam-service/src/test/java/com/inncontrol/iam/AuthenticationControllerTest.java
@@ -1,4 +1,18 @@
 package com.inncontrol.iam;
 
+import com.inncontrol.iam.domain.services.UserCommandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AuthenticationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserCommandService userCommandService;
 }
