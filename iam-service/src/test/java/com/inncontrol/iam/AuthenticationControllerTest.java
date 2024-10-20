9a7e339e39e8a288c7c83c4542cd398fb4a4c831
ern23zc <U202112907@upc.edu.pe>
2024-10-19 19:44:30 -0500

    fix: Added missing references

--- iam-service/src/test/java/com/inncontrol/iam/AuthenticationControllerTest.java
+++ iam-service/src/test/java/com/inncontrol/iam/AuthenticationControllerTest.java
@@ -1,11 +1,20 @@
 package com.inncontrol.iam;
 
+import com.inncontrol.iam.domain.model.commands.RefreshTokenCommand;
 import com.inncontrol.iam.domain.services.UserCommandService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
