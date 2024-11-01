43f730642134048552b3361678d1890a3275ddcf
ern23zc <U202112907@upc.edu.pe>
2024-10-31 23:55:25 -0500

    feat: Add Controller

--- employees-service/src/main/java/com/inncontrol/employees/controller/EmployeeController.java
+++ employees-service/src/main/java/com/inncontrol/employees/controller/EmployeeController.java
@@ -1,4 +1,25 @@
 package com.inncontrol.employees.controller;
 
+import com.inncontrol.employees.application.EmployeeService;
+import com.inncontrol.employees.domain.Employee;
+import com.inncontrol.employees.dto.EmployeeDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> registerEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee savedEmployee = employeeService.registerEmployee(employeeDTO);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
 }
