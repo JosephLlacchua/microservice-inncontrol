ae9dd04044ed28d38b4f40e10926eebd97af100a
ern23zc <U202112907@upc.edu.pe>
2024-10-31 23:56:27 -0500

    feat: Add Data Transfer Object

--- employees-service/src/main/java/com/inncontrol/employees/dto/EmployeeDTO.java
+++ employees-service/src/main/java/com/inncontrol/employees/dto/EmployeeDTO.java
@@ -1,4 +1,31 @@
 package com.inncontrol.employees.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeDTO {
+
+    @NotBlank
+    @Size(max = 50)
+    private String nombre;
+
+    @NotBlank
+    @Size(max = 50)
+    private String apellido;
+
+    @NotBlank
+    private String genero;
+
+    @Email
+    @NotBlank
+    @Size(max = 50)
+    private String correo;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{9}$", message = "El teléfono debe tener 9 dígitos")
+    private String telefono;
+
+    // Getters y Setters
 }
