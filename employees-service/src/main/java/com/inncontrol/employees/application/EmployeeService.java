94952f69ad8bded9c5553a23777dfa1e72f4d690
ern23zc <U202112907@upc.edu.pe>
2024-10-31 23:55:51 -0500

    feat: Employee Service

--- employees-service/src/main/java/com/inncontrol/employees/application/EmployeeService.java
+++ employees-service/src/main/java/com/inncontrol/employees/application/EmployeeService.java
@@ -1,4 +1,29 @@
 package com.inncontrol.employees.application;
 
+import com.inncontrol.employees.domain.Employee;
+import com.inncontrol.employees.domain.EmployeeGender;
+import com.inncontrol.employees.dto.EmployeeDTO;
+import com.inncontrol.employees.domain.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Transactional
+    public Employee registerEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        employee.setNombre(employeeDTO.getNombre());
+        employee.setApellido(employeeDTO.getApellido());
+        employee.setGenero(EmployeeGender.valueOf(employeeDTO.getGenero()));
+        employee.setCorreo(employeeDTO.getCorreo());
+        employee.setTelefono(employeeDTO.getTelefono());
+        return employeeRepository.save(employee);
+    }
 }
