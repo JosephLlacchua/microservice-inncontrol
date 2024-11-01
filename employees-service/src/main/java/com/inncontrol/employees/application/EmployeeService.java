19fdbc1355ae5e57a9f443cbcba0c8c05b7b2064
ern23zc <U202112907@upc.edu.pe>
2024-11-01 00:26:05 -0500

    feat: Update file

--- employees-service/src/main/java/com/inncontrol/employees/application/EmployeeService.java
+++ employees-service/src/main/java/com/inncontrol/employees/application/EmployeeService.java
@@ -7,6 +7,9 @@ import com.inncontrol.employees.domain.EmployeeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
 
@@ -25,5 +28,30 @@ public class EmployeeService {
         employee.setCorreo(employeeDTO.getCorreo());
         employee.setTelefono(employeeDTO.getTelefono());
         return employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Transactional
+    public Optional<Employee> updateEmployee(Long id, EmployeeDTO employeeDTO) {
+        return employeeRepository.findById(id).map(employee -> {
+            employee.setNombre(employeeDTO.getNombre());
+            employee.setApellido(employeeDTO.getApellido());
+            employee.setGenero(EmployeeGender.valueOf(employeeDTO.getGenero()));
+            employee.setCorreo(employeeDTO.getCorreo());
+            employee.setTelefono(employeeDTO.getTelefono());
+            return employeeRepository.save(employee);
+        });
+    }
+
+    @Transactional
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
     }
 }
