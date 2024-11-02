d779ad67fe749145508918fe0a02f9f7dec07dd7
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2024-11-02 14:28:36 -0500

    Feat:I added the respective validations and mappings of the classes and Dto classes.

---
+++ communications-service/src/main/java/com/inncontrol/communications/controller/MessageController.java
@@ -0,0 +1,52 @@
+package com.inncontrol.communications.controller;
+
+import com.inncontrol.communications.application.MessageService;
+import com.inncontrol.communications.domain.Message;
+import com.inncontrol.communications.dto.MessageDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+@Tag(name = "Messages Management", description = "Operations pertaining to messages management")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Operation(summary = "Create a new message")
+    @PostMapping
+    public ResponseEntity<Message> createMessage(@RequestBody MessageDto messageDto) {
+        Message createdMessage = messageService.createMessage(messageDto);
+        return ResponseEntity.ok(createdMessage);
+    }
+
+    @Operation(summary = "Get all messages")
+    @GetMapping
+    public ResponseEntity<List<Message>> getAllMessages() {
+        return new ResponseEntity<>(messageService.getAllMessages(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get messages by receiver")
+    @PutMapping("/{id}")
+    public ResponseEntity<Message> updateMessage(@PathVariable Long id, @RequestBody MessageDto messageDto) {
+        return messageService.updateMessage(id, messageDto)
+                .map(updatedMessage -> new ResponseEntity<>(updatedMessage, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Operation(summary = "Get messages by receiver")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMessage(@PathVariable Long id) {
+        messageService.deleteMessage(id);
+        return ResponseEntity.noContent().build();
+    }
+}
