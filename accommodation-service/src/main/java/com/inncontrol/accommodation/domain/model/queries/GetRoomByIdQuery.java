56440c8c4ca7446ee78eb3bfe42701c24c41e0a7
SharonBarrial <u202114900@upc.edu.pe>
2024-11-01 22:23:21 -0500

    feat: add components file (accommodation-service)

---
+++ accommodation-service/src/main/java/com/inncontrol/accommodation/domain/model/queries/GetRoomByIdQuery.java
@@ -0,0 +1,8 @@
+package com.inncontrol.accommodation.domain.model.queries;
+
+/**
+ * @author Sharon Antuanet Ivet Barrial Marin - u202114900
+ * @version 1.0
+ */
+public record GetRoomByIdQuery() {
+}
