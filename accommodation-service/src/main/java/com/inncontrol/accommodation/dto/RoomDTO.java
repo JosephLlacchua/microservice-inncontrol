ea1a9b330873dd537828ca617547b29a87f7e7d1
SharonBarrial <u202114900@upc.edu.pe>
2024-11-01 23:59:52 -0500

    feat: update roomDTO (dto)

--- accommodation-service/src/main/java/com/inncontrol/accommodation/dto/RoomDTO.java
+++ accommodation-service/src/main/java/com/inncontrol/accommodation/dto/RoomDTO.java
@@ -1,8 +1,28 @@
 package com.inncontrol.accommodation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Sharon Antuanet Ivet Barrial Marin - u202114900
  * @version 1.0
  */
+
+@Setter
+@Getter
 public class RoomDTO {
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private int roomNumber;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String roomType;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String roomStatus;
 }
